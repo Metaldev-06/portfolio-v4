936376fcd8f22a2cc44d4ef65ffaa3e3bf5b9ca2
Metaldev-06 <fernydiaz62@gmail.com>
2023-11-09 19:22:21 -0300

    Add: deploy

--- portfolio-v4/src/environments/environment.development.ts
+++ portfolio-v4/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   environment: 'development',
   production: false,
-  apiUrl: 'http://localhost:1337/api',
+  // apiUrl: 'http://localhost:1337/api',
+  apiUrl: 'https://portfolio-v4-production.up.railway.app/api',
 };
